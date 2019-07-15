1ce367158468e0e96cc3f62635893d35d04210db
lucazweb <lucazwebmail@gmail.com>
2019-07-15 14:37:47 -0300

    Change image gallery displayed admin name

--- theme/models.py
+++ theme/models.py
@@ -1,10 +1,9 @@
 from django.db import models
 from mezzanine.blog.models import BlogPost
 
-
 class Slider(models.Model):
     post = models.ForeignKey(BlogPost)
     photo = models.ImageField()
 
-    # def __str__(self):
-    #     return self.photo
+    def __str__(self):
+        return self.post, self.post.publish_date.strftime("%d/%m/%Y"),   self.photo
