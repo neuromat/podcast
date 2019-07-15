22834e03551984f4bbe635f515deb1e08b95962c
lucazweb <lucazwebmail@gmail.com>
2019-07-15 14:45:35 -0300

    Revert / Change image gallery displayed admin name

--- theme/models.py
+++ theme/models.py
@@ -5,6 +5,6 @@ from mezzanine.blog.models import BlogPost
 class Slider(models.Model):
     post = models.ForeignKey(BlogPost)
     photo = models.ImageField()
-
+ 
     # def __str__(self):
     #     return self.photo
