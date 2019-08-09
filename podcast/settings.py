4dbd449840ee0748cc3487251fb099051d97c56f
lucazweb <lucazwebmail@gmail.com>
2019-08-09 16:41:05 -0300

    Adds deployment specific files to gitignore

--- podcast/settings.py
+++ podcast/settings.py
@@ -91,6 +91,10 @@ EXTRA_MODEL_FIELDS = (
     # ),
 )
 
+MIGRATION_MODULES = {
+   "blog": "migrations_mezzanine_blog",
+}
+
 # Setting to turn on featured images for blog posts. Defaults to False.
 #
 # BLOG_USE_FEATURED_IMAGE = True
@@ -157,17 +161,17 @@ FILE_UPLOAD_PERMISSIONS = 0o644
 DATABASES = {
     "default": {
         # Add "postgresql_psycopg2", "mysql", "sqlite3" or "oracle".
-        "ENGINE": "django.db.backends.",
+        "ENGINE": "django.db.backends.sqlite3",
         # DB name or path to database file if using sqlite3.
-        "NAME": "",
+        "NAME": "db.sqlite3",
         # Not used with sqlite3.
-        "USER": "",
-        # Not used with sqlite3.
-        "PASSWORD": "",
-        # Set to empty string for localhost. Not used with sqlite3.
-        "HOST": "",
-        # Set to empty string for default. Not used with sqlite3.
-        "PORT": "",
+        # "USER": "",
+        # # Not used with sqlite3.
+        # "PASSWORD": "",
+        # # Set to empty string for localhost. Not used with sqlite3.
+        # "HOST": "",
+        # # Set to empty string for default. Not used with sqlite3.
+        # "PORT": "",
     }
 }
 
